f644011c589041b447bbb9ed62d806e5dc824fc6
Barbora Petříková <bb.petrikova@gmail.com>
2020-11-01 13:14:41 +0100

    Order Button working

--- src/index.js
+++ src/index.js
@@ -7,10 +7,26 @@ const navigace = () => {
   document.querySelector('nav').classList.toggle('nav-closed');
 };
 
-const hamburger = document.querySelector('#nav-btn');
-hamburger.addEventListener('click', navigace);
+const hamburgerElm = document.querySelector('#nav-btn');
+hamburgerElm.addEventListener('click', navigace);
 
 const navLinks = document.querySelectorAll('nav a');
 navLinks.forEach((navLink) => {
   navLink.addEventListener('click', navigace);
+});
+
+let objednat = false;
+
+const orderBtn = document.querySelector('.order-btn');
+const drinkCup = document.querySelector('.drink__cup');
+orderBtn.addEventListener('click', () => {
+  if (objednat) {
+    drinkCup.classList.toggle('drink__cup--selected');
+    orderBtn.textContent = 'Objednat';
+    objednat = false;
+  } else {
+    drinkCup.classList.toggle('drink__cup--selected');
+    orderBtn.textContent = 'Zrušit';
+    objednat = true;
+  }
 });
