73707f70bbe9762905d5fe9767488f4f63beceea
Barbora Petříková <bb.petrikova@gmail.com>
2020-11-01 12:37:58 +0100

    close nav and hamburger menu

--- src/index.js
+++ src/index.js
@@ -2,3 +2,15 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
+
+const navigace = () => {
+  document.querySelector('nav').classList.toggle('nav-closed');
+};
+
+const hamburger = document.querySelector('#nav-btn');
+hamburger.addEventListener('click', navigace);
+
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach((navLink) => {
+  navLink.addEventListener('click', navigace);
+});
